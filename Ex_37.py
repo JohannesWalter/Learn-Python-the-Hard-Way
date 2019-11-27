e286819c9477e83306ca956aef2281a70d9d4419
JohannesWalter <50334143+JohannesWalter@users.noreply.github.com>
2019-11-27 14:02:05 +0100

    added lists

--- Ex_37.py
+++ Ex_37.py
@@ -27,13 +27,36 @@
 # print("Hello there \v everyone")
 # print("Hello there \a everyone")
 
-my_dict = {'sword_1': "long_sword", 'sword_2': "dagger", 'wand': "simple_wood", 'wand_power': 15}
-print(my_dict)
-print(my_dict["sword_1"])
+# my_dict = {'sword_1': "long_sword", 'sword_2': "dagger", 'wand': "simple_wood", 'wand_power': 15}
+# print(my_dict)
+# print(my_dict["sword_1"])
 
-x = b"hello"
+# x = b"hello"
 
-print(x)
+# print(x)
 
-my_list = [1, 2, "hello", [3, 4, 5]]
-print(my_list)
+# my_list = [1, 2, "hello", [3, 4, 5]]
+# print(my_list[1])
+
+import pandas as pd
+
+# https://www.geeksforgeeks.org/different-ways-to-create-pandas-dataframe/
+
+# Initialize data in lists:
+my_data = {'Name': ["Dominik", "Sebastian", "Johannes", "Irene", "Achim", "Nicolas"], 'Age': [36, 27, 29, 45, 56, 24], 'Titel': ["Head of JRG", "PhD-Candidate", "PhD-Candidate", "Professor", "President", "Hiwi"]}
+
+# create dataframe
+df = pd.DataFrame(my_data)
+
+# prnt the output
+print(df, "\n\n")
+
+# print(df.iloc[0][0])
+# print("\n")
+# print(df.iloc[2][2])
+# print(df["Age"])
+# print(df.iloc[0][2])
+# print(df.iloc[[0]])
+# print(df.iloc[[0, 1]])
+# print(df.iloc[1,2])
+print(df.iloc[:,:])
