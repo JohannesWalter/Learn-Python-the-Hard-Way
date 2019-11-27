65d046c1681156713ce1d5aa598f62a30f8ec419
JohannesWalter <50334143+JohannesWalter@users.noreply.github.com>
2019-11-27 12:05:41 +0100

    27.11.2019 updates

--- Ex_37.py
+++ Ex_37.py
@@ -18,12 +18,22 @@
 
 # sss(4)
 
-print("Hello there \a everyone")
-print("Hello there \b everyone")
-print("Hello there \f everyone")
-print("Hello there \n everyone")
-print("Hello there \r everyone")
-print("Hello there \t everyone")
-print("Hello there \v everyone")
-print("Hello there \a everyone")
+# print("Hello there \a everyone")
+# print("Hello there \b everyone")
+# print("Hello there \f everyone")
+# print("Hello there \n everyone")
+# print("Hello there \r everyone")
+# print("Hello there \t everyone")
+# print("Hello there \v everyone")
+# print("Hello there \a everyone")
 
+my_dict = {'sword_1': "long_sword", 'sword_2': "dagger", 'wand': "simple_wood", 'wand_power': 15}
+print(my_dict)
+print(my_dict["sword_1"])
+
+x = b"hello"
+
+print(x)
+
+my_list = [1, 2, "hello", [3, 4, 5]]
+print(my_list)
