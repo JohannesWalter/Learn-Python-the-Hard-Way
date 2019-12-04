326b893fa13b80796fe14ee8c86708d21f12e158
JohannesWalter <50334143+JohannesWalter@users.noreply.github.com>
2019-12-04 15:55:56 +0100

    Ex_40.py

--- Ex_40.py
+++ Ex_40.py
@@ -45,4 +45,36 @@ made_up_lyrics = Song(["I made up these lines",
 
 made_up_lyrics.sing_me_a_song()
 
+more_lyrics = Song(["howdy", "hows it going", "yeah"])
+
+more_lyrics.sing_me_a_song()
+
+print(more_lyrics.lyrics)
+
+
+
+
+# class Enemy(object):
+#     """A class for creating enemies"""
+
+#     def __init__(self, hp, dp, name):
+#         self.hp = hp
+#         self.dp = dp
+#         self.name = name
+
+#     def info(self):
+#         print(f"My name is {self.name} and I am {self.hp} health points strong! I can deal {self.dp} of damage!")
+
+#     def attack(self):
+
+    
+# Oger = Enemy(100, 50, "Shreck")
+# Oger.info()
+
+
+ly_var = ["these", "are", "separate lyrics"]
+
+sep_lyr = Song(ly_var)
+sep_lyr.sing_me_a_song()
+
 
